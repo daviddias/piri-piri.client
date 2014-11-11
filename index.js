860ccd82697ff1159b7334fe4e7afd06c9202d3c
David Dias <daviddias.p@gmail.com>
2014-11-11 06:29:31 +0000

    add date and machine id

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ var io = require('socket.io-client');
 exports = module.exports;
 
 var ioClient;
+var ppId;
 
 exports.start = function(options, cb) {
   var url = (options.url || 'http://localhost:9876');
   ioClient = io(url);
 
-  ioClient.on('connect', function (){
-    cb();
-  });
+  ppId = ((~~(Math.random() * 1e9)).toString(36) + Date.now());
+
+  ioClient.on('connect', cb);
 };
 
 exports.registerAction = function (command, action) {
@@ -18,8 +19,13 @@ exports.registerAction = function (command, action) {
 };
 
 exports.sendMessage = function (message) {
-  // TODO populate message with date from this machine;
-  ioClient.emit('message', message);
+  var envelope = {
+    message: message,
+    date: Date.now(),
+    ppId: ppId
+  };
+  console.log('SENDING MESSAGE');
+  ioClient.emit('message', envelope);
 };
 
 // TODO
